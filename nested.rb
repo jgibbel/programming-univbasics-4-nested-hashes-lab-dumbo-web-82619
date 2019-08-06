b55c66b9200ff5e61acddef5c2068ad2e792d185
James Gibbel <jamesgibbel@gmail.com>
2019-08-06 01:13:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,6 +126,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  thisfuckingarray = programmer_hash[:dennis_ritchie][:languages]
-  thisfuckingarray[1] ="Assembly"
+  programmer_hash[:dennis_ritchie][:languages][1] ="Assembly"
 end
